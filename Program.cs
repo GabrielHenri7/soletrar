ff016485717dc938232c1e190ed86fdfbe4cb661
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-05 21:38:35 -0300

    Atividade Concluída

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("--- Soletrar ---");
+
+Console.Write(" Digite uma palavra a ser soletrada ");
+string palavra = Console.ReadLine()!;
+
+var caracteres = palavra.ToCharArray ();
+string palavraSoletrada = string.Join('-', caracteres) . ToUpper();
+
+Console.WriteLine($"\nSoletrando \" {palavra.ToUpper()}\": {palavraSoletrada} ");
